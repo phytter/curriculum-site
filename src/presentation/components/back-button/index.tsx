61e53a809dedcd1670c52968cfe36ffd06882de8
alexandre@simpleagro <phytter@hotmail.com>
2022-07-05 09:48:48 -0300

    style: improve UI

--- src/presentation/components/back-button/index.tsx
+++ src/presentation/components/back-button/index.tsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom'
     const navigate = useNavigate()
     return (
       <button
-        className='text-white font-mono text-xs tracking-widest font-semibold flex gap-5 items-center border-b-2 border-b-amber-500 pl-4 pr-24 hover:opacity-70'
+        className='text-white font-mono text-xs tracking-widest font-semibold flex gap-5 items-center border-b-2 border-b-amber-500 pl-4 pr-4 sm:pr-24 hover:opacity-70'
         onClick={() => navigate(-1)}
       >
         <CgArrowLongLeft size={20} className="text-amber-500" /> BACK
--- src/presentation/components/content-header/index.tsx
+++ src/presentation/components/content-header/index.tsx
@@ -4,7 +4,7 @@ const ContentHeader= ({ title = '' }) => {
     return (
 			<div className="flex justify-between mb-10 items-baseline">
 				<BackButton />
-				<h1 className="text-zinc-700	text-5xl font-bold md:pr-20">
+				<h1 className="text-zinc-700	text-4xl sm:text-5xl font-bold md:pr-20">
 					{title}
 				</h1>
 			</div>
